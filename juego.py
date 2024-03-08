a64e50268ae7adc9d2bac918530a737615230472
Emilio Madrid <emadridabarca@gmail.com>
2024-03-08 16:44:40 -0300

    Cierre de funciones extra para calculos y acciones

--- juego.py
+++ juego.py
@@ -7,9 +7,32 @@ def iniciar_juego():
     jugador = Personaje(nombre)
     orco = Personaje("Orco")
     
-    print(jugador.nombre)
-    print(jugador.nivel)
-    print(jugador.experiencia)
+    print(jugador)
 
+    while True:
+        print("\n¡Oh no!, ¡Ha aparecido un Orco!")
+        probabilidad = jugador.probabilidad_vs(orco)
+        print(f"\nCon tu nivel actual, tienes {probabilidad}% de probabilidades de ganarle al Orco.\n")
+        print("Si ganas, ganarás 50 puntos de experiencia y el orco perderá 30.")
+        print("Si pierdes, perderás 30 puntos de experiencia y el orco ganará 50. \n")
+        
+        accion = input("¿Qué deseas hacer? \n1. Atacar \n2. Huir \n")
+        
+        if accion == "1":
+            resultado = "Gana" if uniform(0, 1) <= probabilidad / 100 else "Pierde"
+            if resultado == "Gana":
+                print("\n¡Le has ganado al orco, felicidades! \n")
+                jugador.asignar_exp(50)
+                orco.asignar_exp(-30)
+            else:
+                print("\n¡Oh no! ¡El orco te ha ganado! \n")
+                jugador.asignar_exp(-30)
+                orco.asignar_exp(50)
+                
+            print(jugador)
+            print(orco)
+        elif accion == "2":
+            print("\n¡Has huido! El orco ha quedado atrás.")
+            break
 
 iniciar_juego()
--- personaje.py
+++ personaje.py
@@ -3,3 +3,33 @@ class Personaje():
         self.nombre = nombre
         self.nivel = 1
         self.experiencia = 0
+
+    def __str__(self):
+        return f"NOMBRE: {self.nombre} NIVEL: {self.nivel} EXP: {self.experiencia}"
+
+    def asignar_exp(self, experiencia):
+        temp_exp = self.experiencia + experiencia
+        while temp_exp >= 100:
+            self.nivel += 1
+            temp_exp -= 100
+        while temp_exp < 0 and self.nivel > 1:
+            self.nivel -= 1
+            temp_exp += 100
+        self.experiencia = temp_exp if temp_exp >= 0 else 0
+
+    def __lt__(self, otro):
+        return self.nivel < otro.nivel
+    
+    def __gt__(self, otro):
+        return self.nivel > otro.nivel
+    
+    def __eq__(self, otro):
+        return self.nivel == otro.nivel
+
+    def probabilidad_vs(self, otro):
+        if self > otro:
+            return 66.0
+        elif self < otro:
+            return 33.0
+        else:
+            return 50.0
